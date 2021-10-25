261f995227eb14d7122dc3ad42ea44dad3c51dfa
evan <ejeffries14@gmail.com>
2021-10-24 21:58:39 -0400

    commit tree 3135ba391b39b30d9a72889b7bfb87c1b3f8299f

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    #set :show_exceptions => false
+
+    get '/' do
+        erb :user_input
+    end
+
+    post '/piglatinize' do
+        @text = params[:user_phrase]
+
+        @piglatinze = PigLatinizer.new
+        #erb :piglatinize
+    end
 end
